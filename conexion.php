acd5544a53cccea0f9e248566cc38d85b7853f47
AlanDCG <92465634+AlanDCG@users.noreply.github.com>
2022-04-18 08:18:58 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost", "root","","laboratorio");
+$conexion = mysqli_connect("sql205.epizy.com", "epiz_31542844","wq04Q5W292h0KE","epiz_31542844_laboratorio");
 // Check connection
 if(mysqli_connect_errno()){
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
